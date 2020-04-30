b33b0e8a1843c4736b72eb181fd13376e0e3537f
Yaptaz <62434115+yaptaz@users.noreply.github.com>
2020-04-30 10:42:52 +0200

    second envoi - test non passe a cause de sun

--- player.py
+++ player.py
@@ -17,15 +17,35 @@ class Player:
         self.imbalance={"purchase_cover":[], "sale_cover": []}
 
     def take_decision(self, time):
-
+            duree_pas_de_temps = 0.5
             # TO DO:
             # implement your policy here to return the load charged / discharged in the battery
             # below is a simple example
-            
-            if time>20 and time<35:
-                return -8
-            else:
-                return +5
+
+            if time == 0:
+                return 0
+
+            if time > 0 and time < 18:
+                return -self.battery_stock[time-1]/2*duree_pas_de_temps
+
+            elif time > 17 and time < 36:
+                chargement_batterie = self.sun[time-1] / 2
+                if (self.battery_stock[time-1] + chargement_batterie * duree_pas_de_temps) < self.capacity:
+                    if (chargement_batterie < self.max_load):
+                        return chargement_batterie
+                    else :
+                        chargement_batterie = self.max_load
+                        return chargement_batterie
+                else:
+                    chargement_batterie = (self.capacity - self.battery_stock[time-1]) / duree_pas_de_temps
+                    if (chargement_batterie < self.max_load):
+                        return chargement_batterie
+                    else:
+                        chargement_batterie = self.max_load
+                        return chargement_batterie
+
+            if time > 35 and time < 49:
+                return -self.battery_stock[time-1]/2*duree_pas_de_temps
 
     def update_battery_stock(self, time,load):
             if abs(load) > self.max_load:
